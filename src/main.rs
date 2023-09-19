91b9dcf985547b1a6cc548040a4a0288612f88c0
Adam Eury <euryadam@gmail.com>
2023-09-19 01:03:55 -0400

    Move cpu usage monitoring to a background thread.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
-use std::{error::Error, sync::Arc};
+use std::{
+    error::Error,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
 
 use askama::Template;
 use axum::{extract::State, response::IntoResponse, routing::get, Router, Server};
 use sysinfo::{CpuExt, System, SystemExt};
-use tokio::sync::RwLock;
-
-type SysInfo = Arc<RwLock<System>>;
+use tokio::task::spawn_blocking;
 
 #[derive(Clone)]
 struct AppState {
-    sys: SysInfo,
+    cpus: Arc<RwLock<Vec<Cpu>>>,
 }
 
-impl AppState {
-    async fn get_cpu_usage(&self) -> Vec<Cpu> {
-        let mut sys = self.sys.write().await;
+#[derive(Clone)]
+struct Cpu {
+    id: usize,
+    usage: f32,
+}
+
+fn monitor_cpu_usage(state: AppState) {
+    let mut sys = System::new();
+    sys.refresh_cpu();
+    loop {
         sys.refresh_cpu();
-        let cpus: Vec<Cpu> = sys
+
+        let updated_cpus: Vec<Cpu> = sys
             .cpus()
             .iter()
             .enumerate()
@@ -25,23 +35,26 @@ impl AppState {
                 usage: cpu.cpu_usage(),
             })
             .collect();
-        cpus
+
+        let mut cpus = state.cpus.write().unwrap();
+        *cpus = updated_cpus;
+
+        std::thread::sleep(Duration::from_millis(500));
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut sys = System::new();
-    sys.refresh_cpu();
-
     let app_state = AppState {
-        sys: Arc::new(RwLock::new(sys)),
+        cpus: Arc::new(RwLock::new(Vec::new())),
     };
 
     let router = Router::new()
         .route("/", get(root_handler))
         .route("/cpu-usage", get(get_cpu_usage))
-        .with_state(app_state);
+        .with_state(app_state.clone());
+
+    spawn_blocking(move || monitor_cpu_usage(app_state));
 
     let addr = "0.0.0.0:3000".parse().unwrap();
 
@@ -51,11 +64,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .await?;
 
     Ok(())
-}
-
-struct Cpu {
-    id: usize,
-    usage: f32,
 }
 
 #[derive(Template)]
@@ -71,11 +79,11 @@ struct CpuUsageTemplate {
 }
 
 async fn root_handler(State(state): State<AppState>) -> impl IntoResponse {
-    let cpus = state.get_cpu_usage().await;
+    let cpus = state.cpus.read().unwrap().clone();
     IndexTemplate { cpus }
 }
 
 async fn get_cpu_usage(State(state): State<AppState>) -> impl IntoResponse {
-    let cpus = state.get_cpu_usage().await;
+    let cpus = state.cpus.read().unwrap().clone();
     CpuUsageTemplate { cpus }
 }
